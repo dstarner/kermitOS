43c5e6578cf650f6bd54f128d6cb9c69a9c34a89
Dan Starner <starner.daniel5@gmail.com>
2017-02-20 20:57:05 -0500

    syscall_file began

---
+++ kern/syscall/syscall_file.c
@@ -0,0 +1,23 @@
+#include <types.h>
+#include <syscall.h>
+#include <synch.h>
+#include <vfs.h>
+#include <current.h>
+#include <proc.h>
+#include <kern/errno.h>
+#include <kern/syscall_file.h>
+#include <uio.h>
+
+
+ssize_t
+write(int fd, const void *buf, size_t buflen, int * err) {
+
+}
+
+
+}
+
+ssize_t
+read(int fd, void *buf, size_t buflen, int * err) {
+
+}
