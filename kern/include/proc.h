7fa3e4f9f6edc5fbd451a571152d68ec2e4880c4
Dan Starner <starner.daniel5@gmail.com>
2017-02-22 00:13:36 -0500

    NULL out f_table

--- kern/include/proc.h
+++ kern/include/proc.h
@@ -71,7 +71,7 @@ struct proc {
 	struct vnode *p_cwd;		/* current working directory */
 
 	/* File table...Each process is allowed 128 files open */
-	struct f_handler[__OPEN_MAX] * f_table;
+	struct f_handler *f_table[__OPEN_MAX];
 
 	/* add more material here as needed */
 };
