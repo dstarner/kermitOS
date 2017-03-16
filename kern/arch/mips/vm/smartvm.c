24b9334e25182c2330a90cefa587a29d55c61bc7
Daniel Starner <starner.daniel5@gmail.com>
2017-03-16 21:38:48 +0000

    At least compiles

--- kern/arch/mips/vm/smartvm.c
+++ kern/arch/mips/vm/smartvm.c
@@ -1,3 +1,4 @@
+#include <types.h>
 #include <machine/vm.h>
 #include <vm.h>
 
@@ -9,15 +10,19 @@ void vm_bootstrap() {
 /* Fault handling function called by trap code */
 int vm_fault(int faulttype, vaddr_t faultaddress) {
 
+  (void) faulttype;
+  (void) faultaddress;
+  return 0;
 }
 
 /* Allocate/free kernel heap pages (called by kmalloc/kfree) */
 vaddr_t alloc_kpages(unsigned npages) {
-
+  (void) npages;
+  return 0;
 }
 
 void free_kpages(vaddr_t addr) {
-
+  (void) addr;
 }
 
 /*
@@ -26,10 +31,10 @@ void free_kpages(vaddr_t addr) {
  * to the caller. But it should have been correct at some point in time.
  */
 unsigned int coremap_used_bytes() {
-
+  return 0;
 }
 
 /* TLB shootdown handling called from interprocessor_interrupt */
-void vm_tlbshootdown(const struct tlbshootdown *) {
-
+void vm_tlbshootdown(const struct tlbshootdown * shootdown) {
+  (void) shootdown;
 }
