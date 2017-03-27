8e6dcd0f65d852c88440f92a6d7310891b3d8514
Dan Starner <starner.daniel5@gmail.com>
2017-03-27 19:16:19 -0400

    Added Spinlock

--- kern/arch/mips/vm/mipsvm.c
+++ kern/arch/mips/vm/mipsvm.c
@@ -1,5 +1,13 @@
 #include <types.h>
-#include <machine/vm.h>
+#include <kern/errno.h>
+#include <lib.h>
+#include <spl.h>
+#include <cpu.h>
+#include <spinlock.h>
+#include <proc.h>
+#include <current.h>
+#include <mips/tlb.h>
+#include <addrspace.h>
 #include <vm.h>
 
 /*
